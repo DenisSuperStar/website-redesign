182a2b6ef1169c406a162232d13f54f25b52f60e
Denis <den.shklyarskij@bk.ru>
2022-06-21 22:51:21 +0300

    Импорт контролеров и добавлена маршрутизация

--- app.js
+++ app.js
@@ -10,15 +10,30 @@ import onError from "./config/onError";
 import onListening from "./config/onListening";
 import { settings } from "./config";
 import { __dirname } from "./config/dirname";
+import { loadContactsController } from "./controllers/loadContactsController";
+import { loadTermsController } from "./controllers/loadTermsController";
+import { loadServicesController } from "./controllers/loadServicesController";
+import { loadTypesController } from "./controllers/loadTypesController";
+import { loadIndexController } from "./controllers/loadIndexController";
 
-import { indexRouter } from "./routes/index";
-import { usersRouter } from "./routes/users";
-
+const { request, response, next } = express;
 const { login, password, defaultPort } = settings;
 const port = normalizePort(process.env.PORT || defaultPort);
 const url = `mongodb+srv://${login}:${password}@atlascluster.vylez.mongodb.net/test`;
 const app = express();
 const server = http.createServer(app);
+
+const contactsController = new loadContactsController();
+const termsCotroller = new loadTermsController();
+const serviceController = new loadServicesController();
+const typeController = new loadTypesController();
+const indexController = new loadIndexController();
+
+const { loadContacts } = contactsController;
+const { loadTerms } = termsCotroller;
+const { loadService } = serviceController;
+const { loadTypes } = typeController;
+const { loadIndex } = indexController;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -30,16 +45,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.get("/", loadIndex(request, response, next)); // передать как function(request, response, next) {} 
+app.get("/types", loadTypes(request, response, next));
+app.get("/services", loadService(request, response, next));
+app.get("/terms", loadTerms(request, response, next));
+app.get("/contacts", loadContacts(request, response, next));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (request, response, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, request, response, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -67,5 +85,3 @@ mongoose
 
 server.on("error", onError);
 server.on("listening", onListening);
-
-
