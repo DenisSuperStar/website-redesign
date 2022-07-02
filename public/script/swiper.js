d9efcf2d538afa85bd526517cae5bc4fe26f8179
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-02 14:53:40 +0300

    Добавлены скрипты инициализации для swiper слайдера

---
+++ public/script/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  slidePerView: 1,
+  mousewheel: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
