7151ada0b699846ec5eedc8d6b79bcbec21d6c9a
Denis Shklyarskij <den.shklyarskij@bk.ru>
2022-07-02 17:27:00 +0300

    Изменен css файл

--- public/script/swiper.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  slidePerView: 1,
-  mousewheel: 1,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
